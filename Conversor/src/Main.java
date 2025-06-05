57e846e59f85c9fdc2bc79dc5b04320936abcbc2
Ignacio Velazquez <69434969+IgnacioVelazquez92@users.noreply.github.com>
2025-06-04 23:08:46 -0300

    Switch to Maven build with Gson

--- Conversor/src/Main.java
+++ Conversor/src/Main.java
@@ -1,5 +1,12 @@
+import com.google.gson.Gson;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Example JSON with currency conversion rates
+        String json = "{\"USD\":1.0, \"EUR\":0.91}";
+        Gson gson = new Gson();
+        Map<String, Double> rates = gson.fromJson(json, Map.class);
+        System.out.println("Tasas: " + rates);
     }
 }
